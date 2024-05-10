e638aace6ff94259b64c0990e96630a8bdf92e05
GaizkaCarrasco <gaizka.carrasco@opendeusto.es>
2024-05-10 09:30:14 +0200

    Recorrido sin obstáculos

---
+++ Assets/Script/Movement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Movement : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public Transform[] objetivos;
+    private int currentGoal = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent.SetDestination(objetivos[0].position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(agent.remainingDistance - agent.stoppingDistance == 0 && !agent.pathPending)
+        {
+            currentGoal++;
+        }
+        if(currentGoal == objetivos.Length)
+        {
+            currentGoal = 0;
+        }
+
+        agent.SetDestination(objetivos[currentGoal].position);
+    }
+}
